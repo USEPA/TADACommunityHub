69942061c6a75b170664a505f2a6f90aecf573a8
Kenny Wong <Wong.Kenny@epa.gov>
2025-12-02 16:16:27 -0500

    Draft functions to do data_validation for a few columns
    Currently does data_validation for:
    
    ATTAINS Parameter names
    ATTAINS Organiazation Identifier
    ATTAINS Use names
    WQX Characteristic Name
    
    Also created a function to run data validation checks on all data frame in inst/extdata

--- R/loadNewData.R
+++ R/loadNewData.R
@@ -6,6 +6,10 @@
 #' domain values or not. If not, identify which are not valid.
 #' @export
 #' 
+#' @examples
+#' data("UTAHDWQ")
+#' validateATTAINSParam(UTAHDWQ)
+#' 
 validateATTAINSParam <- function(data) {
   # Load or read data if a file path is provided
   if (is.character(data)) {
@@ -58,6 +62,10 @@ validateATTAINSParam <- function(data) {
 #' domain values or not. If not, identify which are not valid.
 #' @export
 #' 
+#' @examples
+#' data("UTAHDWQ")
+#' validateWQXChar(UTAHDWQ)
+#' 
 validateWQXChar <- function(data) {
   # Load or read data if a file path is provided
   if (is.character(data)) {
@@ -95,13 +103,128 @@ validateWQXChar <- function(data) {
   }
   
   # add values to list
-  result2$issues <- unique(validate::violating(submitted_data, out2)[,"ATTAINS.ParameterName"])
+  result2$issues <- unique(validate::violating(submitted_data, out2)[,"TADA.CharacteristicName"])
   result2$nrows_fails <- report2$fails
   result2$nrows_passes <- report2$passes
   
   return(result2)
 }
 
+
+
+#' Load User Data - Validate ATTAINS Use Names
+#'
+#' Loads a data frame provided by the user.
+#' @param data a R data frame. Future dev will allow other data file types.
+#' @return A list returning if all ATTAINS Use Names are current valid
+#' domain values or not. If not, identify which are not valid.
+#' @export
+#' 
+#' @examples
+#' data("UTAHDWQ")
+#' validateATTAINSUse(UTAHDWQ)
+#' 
+validateATTAINSUse <- function(data) {
+  # Load or read data if a file path is provided
+  if (is.character(data)) {
+    # Example: Read CSV
+    submitted_data <- read.csv(data)
+  } else if (is.data.frame(data)) {
+    submitted_data <- data
+  } else {
+    stop("Input 'data' must be a data frame or a file path.")
+  }
+  
+  rules_values <- validate::validator(
+    toupper(ATTAINS.UseName) %in% toupper(spsUtil::quiet(rExpertQuery::EQ_DomainValues("use_name")[,"code"]))
+  )
+  
+  # Confront data with rules
+  out2 <- validate::confront(submitted_data, rules_values)
+  
+  # Generate validation report
+  report2 <- validate::summary(out2)
+  
+  # Determine acceptance/rejection
+  if (all( validate::values(out2))) { # Example: All rules passed
+    result2 <- list(status = "Accepted", report = report2)
+  } else {
+    
+    result2 <- list(status = "Rejected", report = report2)
+  }
+  
+  # display message if accepted vs rejected
+  if (result2$status == "Accepted") {
+    result2 <- list(status = "Accepted", message = "ATTAINS.UseName(s) passed all validation checks.")
+  } else {
+    result2 <- list(status = "Rejected", message = "ATTAINS.UseName(s) failed some validation checks. Please review the issues.")
+  }
+  
+  # add values to list
+  result2$issues <- unique(validate::violating(submitted_data, out2)[,"ATTAINS.UseName"])
+  result2$nrows_fails <- report2$fails
+  result2$nrows_passes <- report2$passes
+  
+  return(result2)
+}
+
+
+
+#' Load User Data - Validate ATTAINS Org Names
+#'
+#' Loads a data frame provided by the user.
+#' @param data a R data frame. Future dev will allow other data file types.
+#' @return A list returning if all ATTAINS organization names are current valid
+#' domain values or not. If not, identify which are not valid.
+#' @export
+#' 
+#' @examples
+#' data("UTAHDWQ")
+#' validateATTAINSOrg(UTAHDWQ)
+#' 
+validateATTAINSOrg <- function(data) {
+  # Load or read data if a file path is provided
+  if (is.character(data)) {
+    # Example: Read CSV
+    submitted_data <- read.csv(data)
+  } else if (is.data.frame(data)) {
+    submitted_data <- data
+  } else {
+    stop("Input 'data' must be a data frame or a file path.")
+  }
+  
+  rules_values <- validate::validator(
+    toupper(ATTAINS.OrganizationIdentifier) %in% toupper(spsUtil::quiet(rExpertQuery::EQ_DomainValues("org_id")[,"code"]))
+  )
+  
+  # Confront data with rules
+  out2 <- validate::confront(submitted_data, rules_values)
+  
+  # Generate validation report
+  report2 <- validate::summary(out2)
+  
+  # Determine acceptance/rejection
+  if (all( validate::values(out2))) { # Example: All rules passed
+    result2 <- list(status = "Accepted", report = report2)
+  } else {
+    
+    result2 <- list(status = "Rejected", report = report2)
+  }
+  
+  # display message if accepted vs rejected
+  if (result2$status == "Accepted") {
+    result2 <- list(status = "Accepted", message = "ATTAINS.OrganizationIdentifier(s) passed all validation checks.")
+  } else {
+    result2 <- list(status = "Rejected", message = "ATTAINS.OrganizationIdentifier(s) failed some validation checks. Please review the issues.")
+  }
+  
+  # add values to list
+  result2$issues <- unique(validate::violating(submitted_data, out2)[,"ATTAINS.OrganizationIdentifier"])
+  result2$nrows_fails <- report2$fails
+  result2$nrows_passes <- report2$passes
+  
+  return(result2)
+}
 
 
 #' Validate all data .xlsx in a Folder Path
@@ -111,6 +234,10 @@ validateWQXChar <- function(data) {
 #' @return A list of list of what column name contains the current valid
 #' domain values or not. If not, identify which are not valid.
 #' @export
+#' 
+#' @examples
+#' data("UTAHDWQ")
+#' review <- validateAll(folder_path = "inst/extdata/", validateColumn = validateWQXChar)
 #' 
 validateAll <- function(folder_path = NULL, validateColumn){
   
